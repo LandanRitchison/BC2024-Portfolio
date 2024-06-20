2489d48b5640427f600872ad304f061b4899f310
LandanRitchison <Ritchisonlandan@gmail.com>
2024-06-20 13:00:10 -0400

    Added resume and different buttons

--- portifolio.js
+++ portifolio.js
@@ -1,4 +1,5 @@
 let settings = document.querySelector(".settings-button");
 let myProjects = document.querySelector(".projects-button");
 let aboutMe = document.querySelector(".about-button");
+
 console.log(aboutMe, myProjects, settings);
