337f98735d1a6b7d8eb597596e302bbe1adef378
LandanRitchison <Ritchisonlandan@gmail.com>
2024-06-25 22:26:59 -0400

    Getting ready to make a dropdown instead

--- portifolio.js
+++ portifolio.js
@@ -1,5 +1,26 @@
-let settings = document.querySelector(".settings-button");
-let myProjects = document.querySelector(".projects-button");
-let aboutMe = document.querySelector(".about-button");
+let settingsBtn = document.querySelector(".settings-button");
+let myProjectsBtn = document.querySelector(".projects-button");
+let aboutMeBtn = document.querySelector(".about-button");
+let homeBtn = document.querySelector(".home-button");
+let citationsBtn = document.querySelector(".Citations-button");
 
-console.log(aboutMe, myProjects, settings);
+
+settingsBtn.addEventListener("click", function() {
+   window.location.href = "settings.html";
+});
+
+myProjectsBtn.addEventListener("click", function() {
+    window.location.href = "projects.html";
+});
+
+aboutMeBtn.addEventListener("click", function(){
+    window.location.href = "aboutMe.html";
+});
+
+citationsBtn.addEventListener("click", function() {
+    window.location.href ="citation.html";
+});
+
+homeBtn.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
